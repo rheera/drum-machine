3dd873d7d85d574dbc2ccf91e81bf17205587809
Raman Heera <rheera2@uwo.ca>
2020-07-15 21:59:34 -0400

    Initial Commit
    - made some code to handle key presses for the keys that will play sounds

--- src/App.js
+++ src/App.js
@@ -1,58 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
-}
+//TODO make prettier
 
-export default App;
+
+class MyComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            sound: ''
+        };
+        this.handleEnter = this.handleEnter.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
+    }
+    componentDidMount() {
+        document.addEventListener('keydown', this.handleKeyPress)
+    }
+    componentWillUnmount() {
+        document.removeEventListener('keydown', this.handleKeyPress)
+    }
+    handleEnter(keyCode) {
+        this.setState({
+            sound: this.state.sound + 'You pressed the' + keyCode + 'key! '
+        });
+    }
+    handleKeyPress(event) {
+        switch (event.keyCode) {
+            case 13:
+                this.handleEnter(event.keyCode);
+                break;
+            case 65:
+                this.handleEnter(event.keyCode)
+                break;
+        }
+    }
+    render() {
+        return (
+            <div>
+                <h1>Hi</h1>
+                <h1>{this.state.sound}</h1>
+            </div>
+        );
+    }
+};
+
+
+export default MyComponent;
