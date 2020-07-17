9ccca544130cd813fd706968ba0d38e505f7d144
Raman Heera <rheera2@uwo.ca>
2020-07-16 20:52:23 -0400

    Added clicking to buttons
    - now when you click a button it'll set the state to the same if you pressed the letter of the button

--- src/App.js
+++ src/App.js
@@ -5,14 +5,19 @@ Q, W, E, A, S, D, Z, X, C
 81, 87, 69, 65, 83, 68, 90, 88, 67
  */
 
+const keyCodes = [81, 87, 69, 65, 83, 68, 90, 88, 67];
+const keyLettersQWE = ['Q', 'W', 'E'];
+const keyLettersASD = ['A', 'S', 'D'];
+const keyLettersZXC = ['Z', 'X', 'C'];
+const DEFAULT = "Press a key";
 // TODO make 9 buttons on the left columns and rows like in jQuery lessons
 class MyComponent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            sound: ''
+            sound: '',
+            input: DEFAULT
         };
-        this.handleEnter = this.handleEnter.bind(this);
         this.handleKeyPress = this.handleKeyPress.bind(this);
     }
     componentDidMount() {
@@ -21,65 +26,47 @@ class MyComponent extends React.Component {
     componentWillUnmount() {
         document.removeEventListener('keydown', this.handleKeyPress)
     }
-    handleEnter(keyCode) {
-        this.setState({
-            sound: this.state.sound + 'You pressed the ' + keyCode + ' key! '
-        });
-    }
+
     handleKeyPress(event) {
-        switch (event.keyCode) {
-            // Q
-            case 81:
-                this.handleEnter(event.keyCode);
-                break;
-            // W
-            case 87:
-                this.handleEnter(event.keyCode)
-                break;
-            // E
-            case 69:
-                this.handleEnter(event.keyCode);
-                break;
-            // A
-            case 65:
-                this.handleEnter(event.keyCode)
-                break;
-            // S
-            case 83:
-                this.handleEnter(event.keyCode);
-                break;
-            // D
-            case 68:
-                this.handleEnter(event.keyCode)
-                break;
-            // Z
-            case 90:
-                this.handleEnter(event.keyCode);
-                break;
-            // X
-            case 88:
-                this.handleEnter(event.keyCode)
-                break;
-            // C
-            case 67:
-                this.handleEnter(event.keyCode);
-                break;
-            default:
-                this.handleEnter("bad")
-                break;
+        if (keyCodes.includes(event.keyCode)){
+            this.setState({
+                sound: '',
+                input: event.keyCode
+            });
         }
+    }
+
+    playSound(key) {
+
     }
     render() {
         return (
             <div className={"container-fluid"}>
-                <div>
+                <div className={"d-flex justify-content-md-center align-items-center vh-100"}>
                     <div className={"drum-div row"}>
                         <div className={"sound-buttons border"}>
-
+                            <div id={"qwe"} className={"d-flex justify-content-around"}>
+                                {keyLettersQWE.map(letter => (
+                                    <button className={"btn btn-default"} id={letter + '-btn'} onClick={() =>
+                                        this.setState({sound:'', input:letter.charCodeAt(0)})}>{letter}</button>
+                                ))}
+                            </div>
+                            <div id={"asd"} className={"d-flex justify-content-around"}>
+                                {keyLettersASD.map(letter => (
+                                    <button className={"btn btn-default"} id={letter + '-btn'} onClick={() =>
+                                        this.setState({sound:'', input:letter.charCodeAt(0)})}>{letter}</button>
+                                ))}
+                            </div>
+                            <div id={"zxc"} className={"d-flex justify-content-around"}>
+                                {keyLettersZXC.map(letter => (
+                                    <button className={"btn btn-default"} id={letter + '-btn'} onClick={() =>
+                                        this.setState({sound:'', input:letter.charCodeAt(0)})}>{letter}</button>
+                                ))}
+                            </div>
                         </div>
                         <div className={"sound-info border"}>
                             <h1>Hi</h1>
-                            <h1>{this.state.sound}</h1>
+                            <h1>{this.state.input}</h1>
                         </div>
                     </div>
                 </div>
